ccb76f98fc90bd9bee0efd06e0fbe80eb910a9a3
Sunghayo <notsungha@gmail.com>
2024-10-19 22:14:11 +0800

    Implemented basic email input and name extraction from email addresses.

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,5 +1,25 @@
 """
 Word Occurrences
-Estimate: 20 minutes
+Estimate: 25 minutes
 Actual:    minutes
 """
+
+def main():
+    """Store emails and extract names from them."""
+    email = input("Email: ")
+
+    while email != "":
+        name = extract_name_from_email(email)
+        print(f"Extracted name: {name}")
+        email = input("Email: ")
+
+
+def extract_name_from_email(email):
+    """Extract the user's name from their email."""
+    prefix = email.split('@')[0]
+    parts = prefix.split('.')
+    name = " ".join(parts).title()  # Capitalizes the first letter of each word
+    return name
+
+
+main()
