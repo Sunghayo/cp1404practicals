58c8c8bd58eaf380dcb74137d7108910d7301545
Sunghayo <notsungha@gmail.com>
2024-10-19 23:53:47 +0800

    Added display functionality to show champions and winning countries.

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -10,7 +10,7 @@ def main():
     records = read_wimbledon_data(filename)
     champion_wins = count_champion_wins(records)
     countries = extract_countries(records)
-    # The display function will be implemented in the next step
+    display_results(champion_wins, countries)
 
 
 def read_wimbledon_data(filename):
@@ -40,6 +40,16 @@ def extract_countries(records):
     """Extract the countries of the champions and return a set of unique countries."""
     countries = {record[1] for record in records}  # Using a set for unique countries
     return countries
+
+
+def display_results(champion_wins, countries):
+    """Display the Wimbledon champions and the countries that have won."""
+    print("Wimbledon Champions:")
+    for champion, wins in sorted(champion_wins.items()):
+        print(f"{champion} {wins}")
+
+    print("\nThese {} countries have won Wimbledon:".format(len(countries)))
+    print(", ".join(sorted(countries)))
 
 
 main()
